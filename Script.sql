20a4b62ea54e74e03c5c3c069343a32305c0ab31
HangNT169 <104345338+HangNT169@users.noreply.github.com>
2023-05-02 05:35:27 +0700

    Update Script.sql

--- Script.sql
+++ Script.sql
@@ -1,6 +1,6 @@
-CREATE DATABASE sof203_video;
+CREATE DATABASE sof3021_demo;
 
-USE sof203_video;
+USE sof3021_demo;
 
 GO
 
@@ -17,12 +17,12 @@ CREATE TABLE dong_vat (
 );
 
 GO 
-INSERT INTO sof203_video.dbo.dong_vat
+INSERT INTO sof3021_demo.dbo.dong_vat
 (id, ma, ten, can_nang, gioi_tinh, khu_vuc_song, nam_sinh)
 VALUES(N'A9B3DEC6-387F-498A-9AA5-4D986A2EA79C', N'DV02', N'Cá', 2, 0, N'Dưới nước', 1995);
-INSERT INTO sof203_video.dbo.dong_vat
+INSERT INTO sof3021_demo.dbo.dong_vat
 (id, ma, ten, can_nang, gioi_tinh, khu_vuc_song, nam_sinh)
 VALUES(N'F8F4BB0D-628E-439A-AC69-A1B90E748B80', N'DV03', N'Chó', 15, 0, N'Trong Nhà', 2001);
-INSERT INTO sof203_video.dbo.dong_vat
+INSERT INTO sof3021_demo.dbo.dong_vat
 (id, ma, ten, can_nang, gioi_tinh, khu_vuc_song, nam_sinh)
 VALUES(N'D4085A6E-AB73-4F5F-A0E7-D3A1DC40B0BF', N'DV01', N'Mèo', 10, 1, N'Trong Nhà', 2000);
