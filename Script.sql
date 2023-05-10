bacca3b1f2d701a36c6e479566310784a1a5270b
HangNT169 <104345338+HangNT169@users.noreply.github.com>
2023-05-10 15:33:39 +0700

    Update Script.sql

--- Script.sql
+++ Script.sql
@@ -1,28 +1,56 @@
-CREATE DATABASE sof3021_demo;
+CREATE DATABASE SOF3021_WORKSHOP_DEMO;
 
 GO
-USE sof3021_demo;
+USE SOF3021_WORKSHOP_DEMO;
 GO
 
-CREATE TABLE may_tinh(
-	id UNIQUEIDENTIFIER PRIMARY KEY DEFAULT NEWID(),
-	ma varchar(100),
-	ten nvarchar(100),
-	gia float,
-	bo_nho nvarchar(100),
-	mau_sac nvarchar(100),
-	hang nvarchar(100),
-	mieu_ta nvarchar(100)
-)
+CREATE TABLE hang (
+	id uniqueidentifier DEFAULT NEWID() NOT NULL,
+	ma varchar(20) NULL,
+	ten_hang nvarchar(100) NULL,
+	CONSTRAINT hang_PK PRIMARY KEY (id),
+	CONSTRAINT hang_UN UNIQUE (ma)
+);
 
-INSERT INTO may_tinh (ma, ten, gia, bo_nho, mau_sac, hang, mieu_ta) VALUES
-	('MT1', 'Idea Pad Slim', 18000000, N'512GB', N'Xám', N'Lenovo', N'Laptop văn phòng'),
-	('MT2', 'Aspire 3', 11500000, N'256GB', N'Trắng', N'Acer', N'Laptop văn phòng'),
-	('MT3', 'Vostro 5620', 15000000, N'512GB', N'Xanh', N'Dell', N'Laptop văn phòng'),
-	('MT4', 'Nitro 5 Eagle', 22000000, N'512GB', N'Xám', N'Acer', N'Laptop gamming'),
-	('MT5', 'Predator Helios 300', 42000000, N'1TB', N'Đen', N'Acer', N'Laptop gamming'),
-	('MT6', 'Inspiron 3520', 19000000, N'256GB', N'Xanh', N'Dell', N'Laptop văn phòng'),
-	('MT7', 'G15 5520', 38000000, N'512GB', N'Đen', N'Dell', N'Laptop gamming'),
-	('MT8', 'Legion 5', 25000000, N'512GB', N'Xám', N'Lenovo', N'Laptop gamming'),
-	('MT9', 'Thinkpad T15S', 24000000, N'1TB', N'Đen', N'Lenovo', N'Laptop văn phòng'),
-	('MT10', 'S14 G3', 16000000, N'512GB', N'Xám', N'Lenovo', N'Laptop văn phòng')
+CREATE TABLE may_tinh (
+	id uniqueidentifier DEFAULT NEWID() NOT NULL,
+	hang_id uniqueidentifier NULL,
+	ma varchar(20) NULL,
+	ten_may_tinh nvarchar(100) NULL,
+	gia float NULL,
+	mau_sac nvarchar(100) NULL,
+	mieu_ta nvarchar(100) NULL,
+	CONSTRAINT may_tinh_PK PRIMARY KEY (id),
+	CONSTRAINT may_tinh_UN UNIQUE (ma),
+	CONSTRAINT may_tinh_FK FOREIGN KEY (hang_id) REFERENCES SOF3021_WORKSHOP_DEMO.dbo.hang(id)
+);
+ALTER TABLE may_tinh ADD bo_nho varchar(50) NULL;
+
+INSERT INTO hang
+(id, ma, ten_hang)
+VALUES(N'448C3F2B-FC69-4D18-B13C-1B8BAC6ED489', N'H03', N'Dell');
+INSERT INTO hang
+(id, ma, ten_hang)
+VALUES(N'E6ACC1B3-A79D-4546-9800-7431B96047B3', N'H02', N'Acer');
+INSERT INTO hang
+(id, ma, ten_hang)
+VALUES(N'08FD9D5B-3A99-432A-AEAA-A7C9556D0B72', N'H01', N'Lenovo');
+INSERT INTO hang
+(id, ma, ten_hang)
+VALUES(N'5F8179C4-9F03-47CC-A106-FBED92481EF3', N'H04', N'Macbook');
+
+INSERT INTO may_tinh
+(id, hang_id, ma, ten_may_tinh, gia, mau_sac, mieu_ta, bo_nho)
+VALUES(N'63D0059A-D8AE-42AC-B499-05E1085A6580', N'5F8179C4-9F03-47CC-A106-FBED92481EF3', N'M05', N'G15 5520', 20000000, N'Đen', N'Laptop văn phòng', N'512GB');
+INSERT INTO may_tinh
+(id, hang_id, ma, ten_may_tinh, gia, mau_sac, mieu_ta, bo_nho)
+VALUES(N'DC928A50-D50B-4867-BFB9-27FC3ACB95D9', N'E6ACC1B3-A79D-4546-9800-7431B96047B3', N'M04', N'Nitro 5 Eagle', 58000000, N'Xám', N'Laptop văn phòng', N'1TB');
+INSERT INTO may_tinh
+(id, hang_id, ma, ten_may_tinh, gia, mau_sac, mieu_ta, bo_nho)
+VALUES(N'F349496B-5587-4D23-A4DE-31FE6586F3F0', N'448C3F2B-FC69-4D18-B13C-1B8BAC6ED489', N'M03', N'Vostro 5620', 18000000, N'Xanh', N'Laptop văn phòng', N'512GB');
+INSERT INTO may_tinh
+(id, hang_id, ma, ten_may_tinh, gia, mau_sac, mieu_ta, bo_nho)
+VALUES(N'E83CA190-D904-4B00-AD9A-A42400C9151E', N'448C3F2B-FC69-4D18-B13C-1B8BAC6ED489', N'M01', N'Idea Pad Slim', 38000000, N'Xám', N'Laptop văn phòng', N'256GB');
+INSERT INTO may_tinh
+(id, hang_id, ma, ten_may_tinh, gia, mau_sac, mieu_ta, bo_nho)
+VALUES(N'207392AC-54A0-4FB5-A6F2-B3AA8E3B5BE7', N'448C3F2B-FC69-4D18-B13C-1B8BAC6ED489', N'M02', N'Aspire ', 25000000, N'Đen', N'Laptop văn phòng', N'512GB');
